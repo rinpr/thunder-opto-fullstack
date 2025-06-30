c9b3a3519e97b6cee51c84ea3b8ce50e6bd2dd06
rinpr <duggerseven@gmail.com>
2025-06-30 15:12:26 +0700

    added Customer, NewCustomer Interface

--- frontend/thunder-opto-next-js/models/customer.ts
+++ frontend/thunder-opto-next-js/models/customer.ts
@@ -7,13 +7,21 @@
 //     sex: boolean;
 // }
 
-interface Customer {
-    id: string;
-    first_name: string;
-    last_name: string;
-    phone: string;
-    age: number;
-    sex: boolean;
-    createdAt: string;
-    updatedAt: string;
-  }
+export interface Customer {
+  id: string;
+  first_name: string;
+  last_name: string;
+  phone: string;
+  age: number;
+  sex: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface NewCustomer {
+  first_name: string;
+  last_name: string;
+  phone: string;
+  age: number;
+  sex: boolean;
+}
